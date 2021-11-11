8718a31a6ea1176e708aec880cca07690ded0594
Raul <rblanco.dam@institutcampalans.net>
2021-11-11 17:34:39 +0100

    Cambios interficie

---
+++ Prueb_GitHub/Prueb_GitHub/Views/afegirTasca.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Prueb_GitHub.Views
+{
+    /// <summary>
+    /// Lógica de interacción para afegirTasca.xaml
+    /// </summary>
+    public partial class afegirTasca : Window
+    {
+        public afegirTasca()
+        {
+            InitializeComponent();
+        }
+    }
+}
