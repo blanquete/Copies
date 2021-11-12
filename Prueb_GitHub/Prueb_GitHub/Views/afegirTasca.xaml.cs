e5ce6d5d5a92077f50a3bc14cb6796a70bb5bb2a
iprats <iprats.dam@institutcampalans.net>
2021-11-12 16:38:03 +0100

    Canvis btn afegir

--- Prueb_GitHub/Prueb_GitHub/Views/afegirTasca.xaml.cs
+++ Prueb_GitHub/Prueb_GitHub/Views/afegirTasca.xaml.cs
@@ -30,24 +30,18 @@ namespace Prueb_GitHub.Views
             {
 
                 //afageix un nou item al listview
-                lvTasca.Items.Add(new Tasca()
-                {
-                    
-                    Id = int.Parse(txt_id.Text),
-                    Nom = txt_nomTasca.Text,
-                    Descripcio = txt_descripcio.Text,
-                    DInici = (DateTime)datepicker_data_inici.SelectedDate,
-                    DFinal = (DateTime)datepicker_data_final.SelectedDate,
-                    Prioritat_id = (cmb_prioritat.SelectedItem as ComboBoxItem).Content.ToString(), //transforma el valor del item seleccionat
-                    Responsable_id = (cmb_responsable.SelectedItem as ComboBoxItem).Content.ToString(), //transforma el valor del item seleccionat
-                    Estat_id = (cmb_estat.SelectedItem as ComboBoxItem).Content.ToString() //transforma el valor del item seleccionat
-                });
-<<<<<<< Updated upstream
+                lvTasca.Items.Add(new Tasca(
+                    int.Parse(txt_id.Text),
+                    txt_nomTasca.Text,
+                    txt_descripcio.Text,
+                    (DateTime)datepicker_data_inici.SelectedDate,
+                    (DateTime)datepicker_data_final.SelectedDate,
+                    (cmb_prioritat.SelectedItem as ComboBoxItem).Content.ToString(), //transforma el valor del item seleccionat
+                    (cmb_responsable.SelectedItem as ComboBoxItem).Content.ToString(), //transforma el valor del item seleccionat
+                    (cmb_estat.SelectedItem as ComboBoxItem).Content.ToString() //transforma el valor del item seleccionat
+                ));
 
                 netejaCamps();
-=======
-                
->>>>>>> Stashed changes
             }
             catch (Exception)
             {
@@ -124,9 +118,9 @@ namespace Prueb_GitHub.Views
         }
 
         private void lvTasca_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
+        {/*
             Console.WriteLine(lvTasca.SelectedItem.ToString());
             lvTasca.GetValue();
-        }
+        */}
     }
 }
