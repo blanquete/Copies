771285783492cac19d0aaeab936ffb13af574adf
Raul <rblanco.dam@institutcampalans.net>
2021-11-18 16:12:26 +0100

    Update BaseDatos.cs

--- Prueb_GitHub/Prueb_GitHub/BBDD/BaseDatos.cs
+++ Prueb_GitHub/Prueb_GitHub/BBDD/BaseDatos.cs
@@ -10,23 +10,40 @@ namespace Prueb_GitHub.BBDD
     {
         public static MySqlConnection ObtenerConexion()
         {
-            //dades d'acces al heidiSQL
-            MySqlConnection conectar = new MySqlConnection("server = 127.0.0.1; database = todolist; Uid = root; pwd = 1234;");
-            conectar.Open();
+            MySqlConnection conectar;
+            try
+            {
+                conectar = new MySqlConnection("server = 127.0.0.1; database = todolist; Uid = root; pwd = 1234;");
+                conectar.Open();
+            }
+            catch (Exception)
+            {
+                throw;  
+            }
+           
             return conectar;
         }
 
         public static int Agregar(Tasca temp)
         {
             int retorno = 0;
-            string Query = string.Format("Insert into tasca(id, nom, descripcio, dataInici, dataFinal, id_prioritat, id_estat, id_responsable) values('" + temp.Id + "', '" + temp.Nom + "','" + temp.Descripcio + "','" + temp.DInici + "','" + temp.DFinal + "','" + int.Parse(temp.Prioritat_id) + "','" + int.Parse(temp.Estat_id) + "','" + int.Parse(temp.Responsable_id) +"');");
-            //temp.Id, temp.Nom, temp.Descripcio, temp.DInici, temp.DFinal, int.Parse(temp.Prioritat_id), int.Parse(temp.Estat_id), int.Parse(temp.Responsable_id)
+            try
+            {
+                string Query = "Insert into tasca(id, nom, descripcio, dataInici, dataFinal, id_prioritat, id_estat, id_responsable) " +
+                "values('" + temp.Id + "', '" + temp.Nom + "','" + temp.Descripcio + "','" + temp.DInici + "','" + temp.DFinal + "','" + 1 +
+                "','" + 1 + "','" + 1 + "');";
 
-            MySqlConnection mySqlConnection = ObtenerConexion();
-            MySqlCommand comando = new MySqlCommand(Query, mySqlConnection);
-            
-            
-            retorno = comando.ExecuteNonQuery();
+                //temp.Id, temp.Nom, temp.Descripcio, temp.DInici, temp.DFinal, int.Parse(temp.Prioritat_id), int.Parse(temp.Estat_id), int.Parse(temp.Responsable_id)
+
+                MySqlConnection mySqlConnection = ObtenerConexion();
+                MySqlCommand comando = new MySqlCommand(Query, mySqlConnection);
+
+                retorno = comando.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                e.Message.ToString();
+            }
 
             return retorno;
 
