7e6f2ebca91c3d46227e66b9988ab3cc43ef08fc
iprats <iprats.dam@institutcampalans.net>
2021-12-02 17:40:16 +0100

    Update MainWindow.xaml.cs

--- Prueb_GitHub/Prueb_GitHub/Views/MainWindow.xaml.cs
+++ Prueb_GitHub/Prueb_GitHub/Views/MainWindow.xaml.cs
@@ -24,7 +24,8 @@ namespace Prueb_GitHub
     /// </summary>
     public partial class MainWindow : Window
     {
-        public afegirTasca w2;
+        public afegirTasca at;
+        public afegirTasca mt;
         Tasca temp;
         public List<Tasca> todo = new List<Tasca>();
         public List<Tasca> doing = new List<Tasca>();
@@ -63,7 +64,7 @@ namespace Prueb_GitHub
 
         private void MenuItem_Click(object sender, RoutedEventArgs e)
         {
-            afegirTasca at = new afegirTasca(this);
+            at = new afegirTasca(this);
             at.Show();
         }
 
@@ -139,58 +140,50 @@ namespace Prueb_GitHub
         //Funcio per seleccionar un item i poder modificar les dades la tasca. 
         private void MenuItem_Modificar(object sender, RoutedEventArgs e)
         {
-            obrir_i_emplenar();
-        }
-        public void obrir()
-        {
-            if (w2.IsActive)
-            {
-                w2.Close();
+            obrirModificar();
 
+            emplenarCampsFinestra();
 
-                w2 = new afegirTasca(this);
-
-                w2.w1 = this;
-                w2.Show();
-            }
+            mt.Focus();
         }
 
-        public void obrir_i_emplenar()
+        public void obrirModificar()
         {
-            if (w2.IsActive)
+            if (at.IsEnabled)
             {
-                w2.Close();
+                at.Close();
 
-                w2 = new afegirTasca(this);
-
-                w2.w1 = this;
-                w2.Show();
+                mt = new afegirTasca(this);
+                mt.Show();
             }
+            else if(mt.IsEnabled)
+            {
+                mt.Close();
 
-            w2.txt_id.Text = temp.Id.ToString();
-            w2.txt_estat.Text = ((int)temp.Estat).ToString();
-            w2.datepicker_data_inici.SelectedDate = temp.DInici;
-
-
-            w2.txt_nomTasca.Text = temp.Nom;
-            w2.txt_descripcio.Text = temp.Descripcio;
-            w2.datepicker_data_final.SelectedDate = temp.DFinal;
-            w2.cmb_prioritat.SelectedIndex = temp.Prioritat_id;
-            w2.cmb_responsable.SelectedIndex = temp.Responsable_id;
-
-            w2.Focus();
+                mt = new afegirTasca(this);
+                mt.Show();
+            }
+            else
+            {
+                mt = new afegirTasca(this);
+                mt.Show();
+            }
         }
 
         //Funcio per seleccionar un item i poder modificar les dades la tasca. 
         public void emplenarCampsFinestra()
         {
-            if (w2.IsActive)
+            if (mt.IsActive)
             {
-                w2.txt_nomTasca.Text = temp.Nom;
-                w2.txt_descripcio.Text = temp.Descripcio;
-                w2.datepicker_data_final.SelectedDate = temp.DFinal;
-                w2.cmb_prioritat.SelectedItem = temp.Prioritat_id;
-                w2.cmb_responsable.SelectedItem = temp.Responsable_id;
+                mt.txt_id.Text = temp.Id.ToString();
+                mt.txt_estat.Text = ((int)temp.Estat).ToString();
+                mt.datepicker_data_inici.SelectedDate = temp.DInici;
+
+                mt.txt_nomTasca.Text = temp.Nom;
+                mt.txt_descripcio.Text = temp.Descripcio;
+                mt.datepicker_data_final.SelectedDate = temp.DFinal;
+                mt.cmb_prioritat.SelectedIndex = temp.Prioritat_id;
+                mt.cmb_responsable.SelectedIndex = temp.Responsable_id;
             }
         }
 
