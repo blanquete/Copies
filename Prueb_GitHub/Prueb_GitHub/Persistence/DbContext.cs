ee7f98e00aedf8e9dc8efdf4a52ef53894b9262d
Raul <rblanco.dam@institutcampalans.net>
2021-12-10 15:56:32 +0100

    update DbContext

--- Prueb_GitHub/Prueb_GitHub/Persistence/DbContext.cs
+++ Prueb_GitHub/Prueb_GitHub/Persistence/DbContext.cs
@@ -12,7 +12,7 @@ namespace Prueb_GitHub.Persistence
         public static MySqlConnection conectar;
         public static MySqlConnection ObtenerConexion()
         {
-            conectar = new MySqlConnection(String.Format("server = 127.0.0.1; database = todolist; Uid = root; pwd = 1234;"));
+            conectar = new MySqlConnection(String.Format("server = 127.0.0.1; database = todolist; Character Set=utf8; Uid = root; pwd = 1234;"));
             conectar.Open();
             return conectar;
         }
